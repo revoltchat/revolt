168fa34eac01d35aae74b96b2b8e535455c24ab8
Paul <paulmakles@gmail.com>
2021-09-04 12:42:02 +0100

    Add labels to new repositories.

--- labels/apply.sh
+++ labels/apply.sh
@@ -7,3 +7,10 @@ labels -c conf.json -t $PAT revoltchat/autumn
 labels -c conf.json -t $PAT revoltchat/january
 labels -c conf.json -t $PAT revoltchat/desktop
 labels -c conf.json -t $PAT revoltchat/vortex
+labels -c conf.json -t $PAT revoltchat/self-hosted
+labels -c conf.json -t $PAT revoltchat/admin-panel
+labels -c conf.json -t $PAT revoltchat/legal
+labels -c conf.json -t $PAT revoltchat/translations
+labels -c conf.json -t $PAT revoltchat/api
+labels -c conf.json -t $PAT revoltchat/revolt.chat
+labels -c conf.json -t $PAT revoltchat/documentation
